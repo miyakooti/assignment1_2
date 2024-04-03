56d07ed4bb65635a3742d4733879ddb871162fe4
Ubuntu <ubuntu@ip-172-31-89-125.ec2.internal>
2024-04-03 00:24:13 +0000

    create login form

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import boto3
+import os
+
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.urandom(24)
+dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
+login_table = dynamodb.Table("login")
+
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = table.get_item(Key={'email': email})
+        if 'Item' in user:
+            if user['Item']['email'] == email:
+                # ログイン成功
+                flash('Login successful!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Invalid username or password', 'danger')
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.route('/') 
 def hello_world():
     return 'Hello my project!' 
+
 
 
 if __name__ == '__main__':
